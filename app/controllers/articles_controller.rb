6a3f97bf2c73449be333fa5e4d9c865b925da8cf
Chiran Rails <chiranmach11@hotmail.com>
2016-07-13 21:54:53 +0000

    add sign in upon signup

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -29,7 +29,7 @@ class ArticlesController < ApplicationController
     #debugger
     #render plain: params[:article].inspect
     @article = Article.new(article_params)
-    @article.user = User.first
+    @article.user = current_user
     if @article.save
       flash[:success] = "Article was Succesfully Created"
       redirect_to article_path(@article)
